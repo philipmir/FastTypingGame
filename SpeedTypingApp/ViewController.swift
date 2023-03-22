89a150c31b8278ba4674212cf5dec77aaf318730
philipmir <philipmir@hotmail.com>
2023-03-22 14:48:15 +0100

    Initial Commit

---
+++ SpeedTypingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeedTypingApp
+//
+//  Created by Philip Miranda on 2023-03-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
